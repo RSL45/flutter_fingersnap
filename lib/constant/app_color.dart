5483bf793dc50e65ff55d6e051b611100873a046
Oliver <oliver@ganguo.hk>
2024-02-04 08:21:31 +0800

    feat：登录页

---
+++ lib/constant/app_color.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+class AppColors {
+  static const white = Color(0x00ffffff);
+  static const black = Color(0x00000000);
+  static const color_686868 = Color(0x00686868);
+}
---
+++ lib/screen/mine/login_screen.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_fingersnap/constant/app_color.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  final TextStyle defaultStyle = const TextStyle(color: AppColors.white);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/bg_login.webp"),
+            fit: BoxFit.fill,
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 0.22.sh),
+            Image.asset(
+              "assets/images/ic_launcher.png",
+              width: 80.w,
+              height: 80.w,
+            ),
+            SizedBox(height: 9.h),
+            Text(
+              AppLocalizations.of(context)!.appName,
+              style: defaultStyle.copyWith(fontSize: 26.sp),
+            ),
+            SizedBox(height: 7.h),
+            Text(
+              AppLocalizations.of(context)!.login_slogan,
+              style: defaultStyle.copyWith(fontSize: 18.sp),
+            ),
+            SizedBox(height: 0.2.sh),
+            AuthLoginBtn(
+                imagePath: "",
+                btnHint: AppLocalizations.of(context)!.login_facebook),
+            SizedBox(height: 20.h),
+            AuthLoginBtn(
+                imagePath: "",
+                btnHint: AppLocalizations.of(context)!.login_google),
+            SizedBox(height: 20.h),
+            AuthLoginBtn(
+                imagePath: "",
+                btnHint: AppLocalizations.of(context)!.login_google),
+            PrivacyTermSpan(),
+          ],
+        ),
+      ),
+      // Image.asset(
+      //   "assets/images/bg_login.webp",
+      //   fit: BoxFit.fill,
+      //   width: double.infinity,
+      //   height: double.infinity,
+      // ),
+    );
+  }
+}
+
+class AuthLoginBtn extends StatefulWidget {
+  const AuthLoginBtn({
+    required this.imagePath,
+    required this.btnHint,
+    super.key,
+  });
+
+  final String imagePath;
+  final String btnHint;
+
+  @override
+  State<AuthLoginBtn> createState() => _AuthLoginBtnState();
+}
+
+class _AuthLoginBtnState extends State<AuthLoginBtn> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 58.w),
+      child: Row(
+        children: [
+          // Image.asset(widget.imagePath),
+          Text(widget.btnHint),
+        ],
+      ),
+    );
+  }
+}
+
+class PrivacyTermSpan extends StatefulWidget {
+  const PrivacyTermSpan({super.key});
+
+  @override
+  State<PrivacyTermSpan> createState() => _PrivacyTermSpanState();
+}
+
+class _PrivacyTermSpanState extends State<PrivacyTermSpan> {
+  final TextStyle linkStyle = const TextStyle(
+      color: AppColors.white,
+      decoration: TextDecoration.underline,
+      decorationColor: AppColors.white);
+
+  final TextStyle defaultStyle = const TextStyle(color: AppColors.color_686868);
+
+  late TapGestureRecognizer _tapUserTerm;
+  late TapGestureRecognizer _tapPrivacyPolicy;
+
+  @override
+  void initState() {
+    super.initState();
+    _tapUserTerm = TapGestureRecognizer()
+      ..onTap = () {
+        print('点击 Term Service');
+      };
+
+    _tapPrivacyPolicy = TapGestureRecognizer()
+      ..onTap = () {
+        print('点击 Privacy Policy');
+      };
+  }
+
+  @override
+  void dispose() {
+    _tapUserTerm.dispose(); // 销毁对象
+    _tapPrivacyPolicy.dispose(); // 销毁对象
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    InlineSpan span = TextSpan(children: [
+      TextSpan(
+          text: AppLocalizations.of(context)!.login_user_term_hint,
+          style: defaultStyle),
+      TextSpan(
+          text: AppLocalizations.of(context)!.login_user_term,
+          style: linkStyle,
+          recognizer: _tapUserTerm),
+      TextSpan(
+        text: AppLocalizations.of(context)!.login_user_term_and,
+        style: defaultStyle,
+      ),
+      TextSpan(
+          text: AppLocalizations.of(context)!.login_privacy_policy,
+          style: linkStyle,
+          recognizer: _tapPrivacyPolicy),
+    ]);
+    return Text.rich(span);
+  }
+}
